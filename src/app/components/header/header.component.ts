32d2e686589893b637ab57fb26d7b4171006eeb3
JS <jjs19930421@gmail.com>
2019-03-18 12:06:56 +0800

    show menu component on login page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  showMenu = false;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goHome() {
+    this.router.navigate(['home']);
+  }
+
+  goMenu() {
+    this.showMenu = true;
+  }
+
+  onCancel() {
+    this.showMenu = false;
   }
 
 }
