96275b10f2d9e0a22bfc586c408d1cb0a7bb6c41
JS <jjs19930421@gmail.com>
2019-03-18 17:27:05 +0800

    sign up design - complete

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  email = '';
+  password = '';
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+
+  }
+}
